f093bdb359f7ed32e6f42b41b6da55d2b3b98062
nahrinoda <nahrinoda@yahoo.com>
2019-08-09 21:53:00 -0400

    Updates

---
+++ precache-manifest.5841e2125155946686ac044162ac3a4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5111a54cec0a23d413baf3886d3329",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29fe946a47f01007694a",
+    "url": "/static/css/main.16efef5e.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "29fe946a47f01007694a",
+    "url": "/static/js/main.1c053185.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
