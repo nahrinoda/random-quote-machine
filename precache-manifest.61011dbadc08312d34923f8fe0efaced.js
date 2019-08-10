a2e6c661f761fa0130532a2cf9c70a8e155f0401
nahrinoda <nahrinoda@yahoo.com>
2019-08-09 22:11:20 -0400

    Updates

---
+++ precache-manifest.61011dbadc08312d34923f8fe0efaced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f875a0ad8df239030a38e5781355219e",
+    "url": "/nahrinoda/random-quote-machine/index.html"
+  },
+  {
+    "revision": "9dbea9b9e444b3693ea2",
+    "url": "/nahrinoda/random-quote-machine/static/css/main.16efef5e.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/nahrinoda/random-quote-machine/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "9dbea9b9e444b3693ea2",
+    "url": "/nahrinoda/random-quote-machine/static/js/main.87cf3dc5.chunk.js"
+  },
+  {
+    "revision": "ac1158d2a10ef12b200f",
+    "url": "/nahrinoda/random-quote-machine/static/js/runtime~main.c01bb530.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5841e2125155946686ac044162ac3a4a.js"
+  "/nahrinoda/random-quote-machine/precache-manifest.61011dbadc08312d34923f8fe0efaced.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nahrinoda/random-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
